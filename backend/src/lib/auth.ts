d9a1be77ca59e99f42377bc7eee008b30ff953fc
Fraimer De La Cruz <fraimerdelacruzjaziel@gmail.com>
2026-01-12 22:28:15 -0400

    xv

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -22,7 +22,7 @@ export const auth = betterAuth({
       },
     }
   },
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:5491",
+  baseURL: process.env.BETTER_AUTH_URL,
   basePath: "/api/auth",
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
